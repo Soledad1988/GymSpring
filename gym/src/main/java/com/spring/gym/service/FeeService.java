1cf9d7d9f9bcbbfffd132cc47d2ad5dcf9684635
Brenda <brentorres26@gmail.com>
2024-06-08 20:53:16 -0300

    Create service fee

---
+++ gym/src/main/java/com/spring/gym/service/FeeService.java
@@ -0,0 +1,39 @@
+package com.spring.gym.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.gym.model.Customer;
+import com.spring.gym.model.Fee;
+import com.spring.gym.repository.CustomerRepository;
+import com.spring.gym.repository.FeeRepository;
+
+@Service
+public class FeeService implements IFeeService{
+	
+	@Autowired
+	private FeeRepository feeRepository;
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	@Override
+	public void create(Fee fee) {
+		feeRepository.save(fee);
+	}
+
+	@Override
+	public List<Fee> listFee() {
+		return feeRepository.findAll();
+	}
+	
+	//asigno cuota
+	public Fee assignFeeToCustomer(Integer customerId, Fee fee) {
+	    Customer customer = customerRepository.findById(customerId).orElseThrow(() -> new RuntimeException("Customer not found"));
+	    fee.setCustomer(customer);
+	    return feeRepository.save(fee);
+	 }
+
+}
---
+++ gym/src/main/java/com/spring/gym/service/IFeeService.java
@@ -0,0 +1,12 @@
+package com.spring.gym.service;
+
+import java.util.List;
+
+import com.spring.gym.model.Fee;
+
+public interface IFeeService {
+
+	public void create(Fee fee);
+    public List<Fee> listFee();
+    public Fee assignFeeToCustomer(Integer customerId, Fee fee);
+}
