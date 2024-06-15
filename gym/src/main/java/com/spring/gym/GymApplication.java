1dda5c0b91b86f512b08a797e3f009592f45fb1d
Brenda <brentorres26@gmail.com>
2024-06-15 19:20:52 -0300

    Cors configuration

--- gym/src/main/java/com/spring/gym/GymApplication.java
+++ gym/src/main/java/com/spring/gym/GymApplication.java
@@ -2,6 +2,9 @@ package com.spring.gym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GymApplication {
@@ -9,5 +12,20 @@ public class GymApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GymApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200") // URL de tu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
 
 }
