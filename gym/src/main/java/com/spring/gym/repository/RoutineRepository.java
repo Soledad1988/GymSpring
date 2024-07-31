fc767621aa1b59e5aaa1ffdb1ff1aeff3124ae45
Brenda <brentorres26@gmail.com>
2024-07-31 19:27:39 -0300

    Change repository routine

--- gym/src/main/java/com/spring/gym/repository/RoutineRepository.java
+++ gym/src/main/java/com/spring/gym/repository/RoutineRepository.java
@@ -1,11 +1,16 @@
 package com.spring.gym.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.spring.gym.model.Category;
 import com.spring.gym.model.Routine;
 
 @Repository
 public interface RoutineRepository extends JpaRepository<Routine, Integer>{
+	
+	List<Routine> findByCategoryIdCategory(Integer idCategory);
 
 }
